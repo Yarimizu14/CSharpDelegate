177d734a4cd1266a65dfaa935f8b9f179364c169
yarimizu.kensei <yarimizu.kensei14@gmail.com>
2015-01-14 21:41:49 +0900

    Eventを試してみた

---
+++ Assets/DelegateSample3.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegateSample3 : MonoBehaviour {
+
+    /*
+     * デフォルトでprivate
+     */
+    // イベント処理用のデリゲート
+    public delegate void TapEventHandler3(string message);
+
+    // EventなしのTapEventHandler
+    public event TapEventHandler3 tapHandlerA;
+    public event TapEventHandler3 tapHandlerB;
+
+    public void OnTapA () {
+        tapHandlerA("DelegateSample3.OnTapA");
+    }
+
+    public void OnTapB () {
+        tapHandlerB("DelegateSample3.OnTapB");
+    }
+}
+
---
+++ Assets/HandlerSample3.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandlerSample3 : MonoBehaviour {
+
+    public DelegateSample3 delegateSample3;
+
+    // Use this for initialization
+    //
+    void Start () {
+        /*
+         * クリックすると両方呼ばれます
+         * '='は使わず'+='または'-='を使う
+         */
+        delegateSample3.tapHandlerA += (string message) => { Debug.Log("----- lambda"); Debug.Log(message); };
+        delegateSample3.tapHandlerA += ButtonHandler;
+    }
+
+    void ButtonHandler (string message) {
+        Debug.Log("----- method");
+        Debug.Log(message);
+
+        /*
+         * 一度クリックした後は、ラムダ式の方のみ呼ばれます
+         */
+        delegateSample3.tapHandlerA -= ButtonHandler;
+    }
+}
